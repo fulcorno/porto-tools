4f229b65a548aa481bf8cbb10aca9a4ad7ebaf7e
fulcorno <fulvio.corno@polito.it>
2015-03-14 23:03:34 +0100

    defined classes for main Porto entities

--- porto/paper.py
+++ porto/paper.py
@@ -1,1 +1,12 @@
 __author__ = 'Fulvio Corno'
+
+
+class Paper:
+
+    def __init__(self, eprintid, title, date, type):
+        self.eprintid = eprintid
+        self.title = title
+        self.date = date
+        self.type = type
+        self.authors = []
+
---
+++ porto/type.py
@@ -0,0 +1,7 @@
+__author__ = 'Fulvio Corno'
+
+class Type:
+
+    def __init__(self, type, types):
+        self.type = type
+        self.types = types
